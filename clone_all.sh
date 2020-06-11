8c555906cc249cea4bafe39929cffc968b506606
jordan <jordan.rouco-miguez@epitech.eu>
2020-06-11 10:14:28 +0200

    AutoPush

--- clone_all.sh
+++ clone_all.sh
@@ -1,3 +1,3 @@
 git config --global credential.helper store
 
-curl -s "https://api.github.com/$3/$1/repos?page=$2&access_token=6ec6b7d959b5d10ac05d65aeab609a819309e8bf&per_page=1000" | grep -w clone_url | grep -o '[^"]\+://.\+.git' | xargs -L1 git clone
+curl -s "https://api.github.com/$3/$1/repos?page=$2&access_token=$4&per_page=1000" | grep -w clone_url | grep -o '[^"]\+://.\+.git' | xargs -L1 git clone

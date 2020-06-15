42fe9b80ec7cb5d099e3992f05626c18e94b425a
jordan <jordan.rouco-miguez@epitech.eu>
2020-06-15 11:03:50 +0200

    Improving git_initiator.sh

--- git_initiator.sh
+++ git_initiator.sh
@@ -3,6 +3,14 @@ workingPath=$1
 repoName=$2
 repoOrigin=$3
 pathToGitignore=$4
+pathToBackup=$5
+whereToBackup=$6
+
+echo 'Backing up files...\n'
+
+cp -r $5 $6
+
+echo 'Backup done\n'
 
 echo 'initializing repo...\n'
 
